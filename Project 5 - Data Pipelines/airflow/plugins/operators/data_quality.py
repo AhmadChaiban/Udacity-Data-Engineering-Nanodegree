7d69002b7e7a1be67018f33c03e93c8e3d881597
Ahmad Chaiban <ahmadchaiban@gmail.com>
2020-05-30 02:17:11 +0300

    Feat (Data quality operator)

--- Project 5 - Data Pipelines/airflow/plugins/operators/data_quality.py
+++ Project 5 - Data Pipelines/airflow/plugins/operators/data_quality.py
@@ -1,3 +1,4 @@
+import logging
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
@@ -8,15 +9,20 @@ class DataQualityOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 table="",
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.table = table
+        self.redshift_conn_id = redshift_conn_id
 
     def execute(self, context):
-        self.log.info('DataQualityOperator not implemented yet')
+        redshift_hook = PostgresHook(self.redshift_conn_id)
+        records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {self.table}")
+        if len(records) < 1 or len(records[0]) < 1:
+            raise ValueError(f"Data quality check failed. {self.table} returned no results")
+        num_records = records[0][0]
+        if num_records < 1:
+            raise ValueError(f"Data quality check failed. {self.table} contained 0 rows")
+        logging.info(f"Data quality on table {self.table} check passed with {records[0][0]} records")
