260fa47e72232ec1d4fdd776d7885a0af95d0471
Ahmad Chaiban <ahmadchaiban@gmail.com>
2020-05-30 17:21:02 +0300

    Refac (data Quality check on mult tables)

--- Project 5 - Data Pipelines/airflow/plugins/operators/data_quality.py
+++ Project 5 - Data Pipelines/airflow/plugins/operators/data_quality.py
@@ -10,19 +10,25 @@ class DataQualityOperator(BaseOperator):
     @apply_defaults
     def __init__(self,
                  redshift_conn_id="",
-                 table="",
+                 tables=[""],
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        self.table = table
+        self.tables = tables
         self.redshift_conn_id = redshift_conn_id
 
     def execute(self, context):
+        """
+        Performing a basic data quality check
+        on an array of tables.
+
+        """
         redshift_hook = PostgresHook(self.redshift_conn_id)
-        records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {self.table}")
-        if len(records) < 1 or len(records[0]) < 1:
-            raise ValueError(f"Data quality check failed. {self.table} returned no results")
-        num_records = records[0][0]
-        if num_records < 1:
-            raise ValueError(f"Data quality check failed. {self.table} contained 0 rows")
-        logging.info(f"Data quality on table {self.table} check passed with {records[0][0]} records")
+        for table in self.tables:
+            records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {table}")
+            if len(records) < 1 or len(records[0]) < 1:
+                raise ValueError(f"Data quality check failed. {table} returned no results")
+            num_records = records[0][0]
+            if num_records < 1:
+                raise ValueError(f"Data quality check failed. {table} contained 0 rows")
+            logging.info(f"Data quality on table {table} check passed with {records[0][0]} records")
